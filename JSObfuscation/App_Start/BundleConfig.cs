fe35e6dcfdd015f1c9c09494f1e5212330daa1a2
sarveshkushwaha <sarveshkumar@hotmail.co.in>
2014-10-15 23:59:34 +0530

    Upload

---
+++ JSObfuscation/App_Start/BundleConfig.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using BundleTransformer.Core.Builders;
+using BundleTransformer.Core.Orderers;
+using BundleTransformer.Core.Resolvers;
+using BundleTransformer.Core.Transformers;
+
+namespace JSObfuscation
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254726
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //This setting is used when if you have specfied the path Using System.web.Optimization.bundle.Cdnpath then it will try to fetch data from there first
+            bundles.UseCdn = true;
+            //NullBuilder class is responsible for prevention of early applying of the item transformations and combining of code.
+            var nullBuilder = new NullBuilder();
+            //StyleTransformer and ScriptTransformer classes produce processing of stylesheets and scripts.
+            var styleTransformer = new StyleTransformer();
+
+            var scriptTransformer = new ScriptTransformer();
+            //NullOrderer class disables the built-in sorting mechanism and save assets sorted in the order they are declared.
+            var nullOrderer = new NullOrderer();
+
+            //create your own scriptbundle 
+
+            var scriptbundleToObfuscate = new Bundle("~/bundles/WebFormsJs");
+            scriptbundleToObfuscate.Include("~/Scripts/WebForms/WebForms.js",
+                  "~/Scripts/WebForms/WebUIValidation.js",
+                  "~/Scripts/WebForms/MenuStandards.js",
+                  "~/Scripts/WebForms/Focus.js",
+                  "~/Scripts/WebForms/GridView.js",
+                  "~/Scripts/WebForms/DetailsView.js",
+                  "~/Scripts/WebForms/TreeView.js",
+                  "~/Scripts/WebForms/WebParts.js");
+            scriptbundleToObfuscate.Builder = nullBuilder;
+            scriptbundleToObfuscate.Transforms.Add(scriptTransformer);
+            scriptbundleToObfuscate.Orderer = nullOrderer;
+            bundles.Add(scriptbundleToObfuscate);
+
+            //bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
+            //      "~/Scripts/WebForms/WebForms.js",
+            //      "~/Scripts/WebForms/WebUIValidation.js",
+            //      "~/Scripts/WebForms/MenuStandards.js",
+            //      "~/Scripts/WebForms/Focus.js",
+            //      "~/Scripts/WebForms/GridView.js",
+            //      "~/Scripts/WebForms/DetailsView.js",
+            //      "~/Scripts/WebForms/TreeView.js",
+            //      "~/Scripts/WebForms/WebParts.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/MsAjaxJs").Include(
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
+
+            // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
